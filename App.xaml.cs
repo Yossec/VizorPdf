6d62e2e15d00846b9d6f3806d0b204f159ce273e
YossecSA <yossecsenati@gmail.com>
2025-07-10 12:55:00 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VizorPdf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using VizorPdf.helper;
+
+namespace VizorPdf
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly PdfToImageConverter _pdfConverter = new PdfToImageConverter();
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadPdfAsImage();
+        }
+        private async void LoadPdfAsImage()
+        {
+            try
+            {
+                // Cambia esta ruta por la ubicación de tu PDF
+                string pdfPath = "C:/Users/HP/Desktop/firma/prueba.pdf";
+
+                // Convertir la primera página (página 0) a imagen
+                var imageSource = await _pdfConverter.ConvertWithPdfium(pdfPath, 0);
+
+                if (imageSource != null)
+                {
+                    pdfImageView.Source = imageSource;
+                }
+                else
+                {
+                    await DisplayAlert("Error", "No se pudo convertir el PDF a imagen", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+            }
+        }
+
+    }
+}
